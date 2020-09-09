b603a592bfce8a3e68f0be3a4e332f01ab8676e7
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-09 14:28:26 +0300

    register user

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 'use strict';
 console.clear();
 
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
+
+
+// Fake Database
+const users = [];
 
 
 /*======================================================== 
     MIDLLEWARES
 ========================================================*/
+app.use(express.urlencoded({ extended: false }))
 app.set('view-engine', 'ejs');
 
 
@@ -15,7 +21,7 @@ app.set('view-engine', 'ejs');
     APP SCRIPT - START
 ========================================================*/
 
-// SETUP HOME ROUTE
+/* ========  GET ROUTES   ======= */
 app.route('/').get((req, res) => {
     res.render('index.ejs', {name: 'Darbaz', title: 'Home'})
 })
@@ -31,6 +37,34 @@ app.route('/register')
         res.render('register.ejs', {title: 'Register'})
     })
 
+/* ========  POST ROUTES   ======= */
+app.post('/register', async (req, res) => {
+    const {name, email, password } = req.body;
+
+    // if ( !name || !email || !password ) {
+    //     res.status(400).send('please fill all fileds')
+    // }
+
+    try {
+        const hashedPass = await bcrypt.hash(password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: name,
+            email: email,
+            password: hashedPass
+        })
+
+        // after user registerd, redirect to login page
+        res.redirect('/login')
+    }
+
+    catch(err) {
+        res.redirect('/register')
+        console.error(err)
+    }
+
+    console.log(users);
+})
 
 /*======================================================== 
     APP SCRIPT - END
